16e7426444f287df5b4cae6b477ca14497dfc9ef
Andree Lindgren <andree.lindgren@gmail.com>
2017-12-04 22:30:36 +0100

    Added GLFW and removed dependency on Win32

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,68 @@
 module Main where
 
-import Lib
-import Platform
+import           Control.Monad
+import qualified Graphics.UI.GLFW as GLFW
+import           Lib
+import           Graphics.GL
+import           Data.Bits ((.|.))
+
+errorCallback :: GLFW.ErrorCallback
+errorCallback error description = do
+    putStrLn $ "Error: " ++ show error
+    putStrLn description
+
+withGLFW :: IO () -> IO ()
+withGLFW program = do
+    GLFW.setErrorCallback (Just errorCallback)
+    initSuccessful <- GLFW.init
+    when initSuccessful $ do
+        program
+        GLFW.terminate
 
 main :: IO ()
-main = withPlatform "mroW" () $ \(state, platform) -> do
-    return (state, False)
+main = do
+    withGLFW $ do
+        maybeWindow <- GLFW.createWindow 960 540 "mroW" Nothing Nothing
+        case maybeWindow of
+            Nothing -> return ()
+            Just window -> do
+                GLFW.makeContextCurrent (Just window)
+                GLFW.setKeyCallback window (Just keyCallback)
+                GLFW.swapInterval 1 -- assuming this enables vsync
+                runLoop window
+                GLFW.destroyWindow window
+
+runLoop :: GLFW.Window -> IO ()
+runLoop window = do
+    GLFW.pollEvents
+    windowShouldClose <- GLFW.windowShouldClose window
+    case windowShouldClose of
+        True -> return ()
+        False -> do
+            -- update state ?
+            -- handle input ?
+            maybeTime <- GLFW.getTime
+            case maybeTime of
+                Nothing -> return ()
+                Just time -> return () -- update the scene or something here...
+
+            -- pre render
+            (width, height) <- GLFW.getFramebufferSize window
+            let aspectRatio = fromIntegral width / fromIntegral height
+            glViewport 0 0 (fromIntegral width) (fromIntegral height)
+            glClearColor 1.0 0.7 0.4 1.0
+            glClear $ GL_COLOR_BUFFER_BIT .|. GL_DEPTH_BUFFER_BIT
+            
+            -- render ?
+
+            -- post render
+            GLFW.swapBuffers window
+            runLoop window
+
+keyCallback :: GLFW.KeyCallback
+keyCallback window key scancode action mods = do
+    when (key == GLFW.Key'Escape) $
+        GLFW.setWindowShouldClose window True
+
+    -- printing key input for testing
+    putStrLn $ show key ++ " - " ++ show scancode ++ " - " ++ show action ++ " - " ++ show mods
