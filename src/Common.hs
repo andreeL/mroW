2958ca88b15f268d1234bce584f0bcf5a0c7c02f
Andree Lindgren <andree.lindgren@gmail.com>
2018-01-03 09:37:08 +0100

    Player now controls acceleration instead of velocity

--- src/Common.hs
+++ src/Common.hs
@@ -4,6 +4,8 @@ import Linear (V3(..), M33(..))
 
 type Time = Double
 type DeltaTime = Float
+type Acceleration = V3 Float
+type Velocity = V3 Float
 type Position = V3 Float
 type Rotation = M33 Float
 type Placement = (Position, Rotation)
--- src/Player.hs
+++ src/Player.hs
@@ -1,4 +1,5 @@
 {-# Language RecordWildCards #-}
+{-# LANGUAGE Arrows #-}
 
 module Player
   ( PlayerInput(..)
@@ -8,8 +9,8 @@ module Player
 
 import Behaviour (Behaviour, bScan)
 import Common
-import Control.Arrow (Arrow(..))
-import Linear (V3(..), (^+^), (^*), normalize)
+import Control.Arrow (Arrow(..), (>>>), returnA)
+import Linear (V3(..), (^+^), (^*), normalize, zero)
 
 data PlayerInput = PlayerInput {
   _time :: Time,
@@ -22,10 +23,27 @@ data PlayerInput = PlayerInput {
 
 type Player = Behaviour PlayerInput Position
 
-createPlayer :: Position -> Player
-createPlayer position = bScan updatePosition position
-  where updatePosition currentPosition PlayerInput{..} =
-          let x = (if _moveRight then 1 else 0) + (if _moveLeft then (-1) else 0)
-              y = (if _moveUp then 1 else 0) + (if _moveDown then (-1) else 0)
-              speed = 2 * _deltaSeconds
-           in (normalize $ V3 x y 0) ^* speed ^+^ currentPosition
+createPlayer position = proc playerInput@PlayerInput{..} -> do
+  let acceleration = thrusterAcceleration playerInput
+  (_, position') <- advancePlayerState (zero, position) -< (_deltaSeconds, acceleration)
+  returnA -< position'
+  
+thrusterAcceleration :: PlayerInput -> Acceleration
+thrusterAcceleration PlayerInput{..} =
+  let x = (if _moveRight then 1 else 0) + (if _moveLeft then (-1) else 0)
+      y = (if _moveUp then 1 else 0) + (if _moveDown then (-1) else 0)
+      strength = 20.0
+  in (normalize $ V3 x y 0) ^* strength
+
+advancePlayerState :: (Velocity, Position) -> Behaviour (DeltaTime, Acceleration) (Velocity, Position)
+advancePlayerState = bScan $ \state (deltaSeconds, thrusterAcceleration) ->
+  integrate deltaSeconds (const thrusterAcceleration) state
+
+integrate :: DeltaTime -> ((Velocity, Position) -> Acceleration) -> (Velocity, Position) -> (Velocity, Position)
+integrate deltaSeconds getAcceleration (velocity, position) =
+  let integratedHalfAcceleration = (getAcceleration (velocity, position)) ^* (0.5 * deltaSeconds)
+      friction = 0.1 ** deltaSeconds
+      intermediateVelocity = (velocity ^* friction) ^+^ integratedHalfAcceleration
+      velocity' = intermediateVelocity ^+^ integratedHalfAcceleration
+      position' = position ^+^ (intermediateVelocity ^* deltaSeconds)
+    in (velocity', position')
